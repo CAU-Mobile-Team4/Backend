944dd3718a285c08b511a42b71c0f26d4be5f15b
zestbear <zestbear90@gmail.com>
2023-11-06 19:32:21 +0900

    Initial Commit

---
+++ src/main/java/Team4/CalendarNLPServer/CalendarNlpServerApplication.java
@@ -0,0 +1,13 @@
+package Team4.CalendarNLPServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalendarNlpServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalendarNlpServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Team4/CalendarNLPServer/CalendarNlpServerApplicationTests.java
@@ -0,0 +1,13 @@
+package Team4.CalendarNLPServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalendarNlpServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
