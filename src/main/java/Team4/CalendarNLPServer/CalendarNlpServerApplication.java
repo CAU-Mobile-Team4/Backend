f8e52f5015e7c8f8a00cb57928c9889d9ce19261
zestbear <zestbear90@gmail.com>
2023-11-09 15:38:00 +0900

    update dependencies

--- src/main/java/Team4/CalendarNLPServer/CalendarNlpServerApplication.java
+++ src/main/java/Team4/CalendarNLPServer/CalendarNlpServerApplication.java
@@ -2,12 +2,16 @@ package Team4.CalendarNLPServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class CalendarNlpServerApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(CalendarNlpServerApplication.class, args);
+
 	}
 
 }
