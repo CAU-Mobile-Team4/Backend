2682dbd0d88eaa2ef438a520e8915f58b220baa8
zestbear <zestbear90@gmail.com>
2023-11-13 19:33:33 +0900

    학생 관련 오류

---
+++ src/main/java/Team4/CalendarNLPServer/common/StudentNOTExistException.java
@@ -0,0 +1,9 @@
+package Team4.CalendarNLPServer.common;
+
+public class StudentNOTExistException extends RuntimeException {
+
+    public StudentNOTExistException(String message) {
+        super(message);
+    }
+
+}
