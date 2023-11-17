119ed3de8e22ce597a8fd9ab43461ed91502595d
zestbear <zestbear90@gmail.com>
2023-11-18 03:08:47 +0900

    @PathVariable 구분 위한 변수명 변경

--- src/main/java/Team4/CalendarNLPServer/controller/ScheduleController.java
+++ src/main/java/Team4/CalendarNLPServer/controller/ScheduleController.java
@@ -1,6 +1,7 @@
 package Team4.CalendarNLPServer.controller;
 
-import Team4.CalendarNLPServer.controller.dto.ScheduleListRequestDto;
+import Team4.CalendarNLPServer.controller.dto.ScheduleListResponseDto;
+import Team4.CalendarNLPServer.controller.dto.ScheduleUpdateRequestDto;
 import Team4.CalendarNLPServer.domain.schedule.Schedule;
 import Team4.CalendarNLPServer.service.schedule.ScheduleService;
 import lombok.RequiredArgsConstructor;
@@ -14,23 +15,34 @@ public class ScheduleController {
 
     private final ScheduleService scheduleService;
 
-    @PostMapping("/schedule/{id}")
-    public Long save(@PathVariable Long id,
+    // stuId 학생의 일정 목록에 저장
+    @PostMapping("/schedule/{stuId}")
+    public Long save(@PathVariable Long stuId,
                      @RequestBody String text) throws Exception {
-        return scheduleService.save(id, text);
+        return scheduleService.save(stuId, text);
     }
 
-    @GetMapping("/schedule/{id}")
-    public List<ScheduleListRequestDto> finaAll(@PathVariable Long id) {
-        return scheduleService.findAllByStuID(id);
+    // schId 일정의 수정
+    @PutMapping("/schedule/{schId}")
+    public Long update(@PathVariable Long schId,
+                       @RequestBody ScheduleUpdateRequestDto requestDto) {
+        return scheduleService.update(schId, requestDto);
     }
 
-    @PostMapping("/schedule/search/{id}")
-    public List<Schedule> search(@PathVariable Long id,
-                                 @RequestBody String keyword) {
-        return scheduleService.findAllByKeyword(keyword);
+    // stuId 학생의 모든 일정 List 반환
+    @GetMapping("/schedule/{stuId}")
+    public List<ScheduleListResponseDto> finaAll(@PathVariable Long stuId) {
+        return scheduleService.findAllByStuID(stuId);
     }
 
+    // stuId 학생의 일정 중에 keyword가 포함되어 있는 일정 List 반환
+    @PostMapping("/schedule/search/{stuId}")
+    public List<ScheduleListResponseDto> search(@PathVariable Long stuId,
+                                                @RequestBody String keyword) {
+        return scheduleService.findAllByKeyword(stuId, keyword);
+    }
+
+    // stuId 학생의 schId 일정 삭제
     @DeleteMapping("/schedule/{stuId}/{schId}")
     public void delete(@PathVariable Long stuId,
                        @PathVariable Long schId) {
