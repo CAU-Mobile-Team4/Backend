dfdddbc5a0d477edeec4a1de3319deec9297861e
zestbear <zestbear90@gmail.com>
2023-11-19 00:57:19 +0900

    controller 역할 주석 추가

--- src/main/java/Team4/CalendarNLPServer/controller/StudentController.java
+++ src/main/java/Team4/CalendarNLPServer/controller/StudentController.java
@@ -11,6 +11,8 @@ public class StudentController {
 
     private final StudentService studentService;
 
+    // 사용자 로그인 및 회원가입
+    // 입력하였을 때 DB에 정보가 있으면 로그인, 정보가 없으면 자동 회원가입
     @PostMapping("/student")
     public Long join(@RequestBody StudentResponseDto responseDto) {
         return studentService.join(responseDto);
