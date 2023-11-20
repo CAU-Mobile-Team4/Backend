1d4d5c45356d759259de257ba75b4c44eae03442
zestbear <zestbear90@gmail.com>
2023-11-21 07:37:19 +0900

    불필요한 import 삭제

--- src/test/java/Team4/CalendarNLPServer/domain/schedule/ScheduleRepositoryTest.java
+++ src/test/java/Team4/CalendarNLPServer/domain/schedule/ScheduleRepositoryTest.java
@@ -2,22 +2,17 @@ package Team4.CalendarNLPServer.domain.schedule;
 
 import Team4.CalendarNLPServer.domain.student.Student;
 import Team4.CalendarNLPServer.domain.student.StudentRepository;
-import org.aspectj.lang.annotation.After;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
-//@ActiveProfiles("test")
 class ScheduleRepositoryTest {
 
     @Autowired
--- src/test/java/Team4/CalendarNLPServer/service/schedule/ScheduleServiceTest.java
+++ src/test/java/Team4/CalendarNLPServer/service/schedule/ScheduleServiceTest.java
@@ -1,16 +1,13 @@
 package Team4.CalendarNLPServer.service.schedule;
 
-import Team4.CalendarNLPServer.controller.dto.StudentResponseDto;
 import Team4.CalendarNLPServer.service.student.StudentService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-//@ActiveProfiles("test")
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 class ScheduleServiceTest {
