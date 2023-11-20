b314e064e4d9b47bdd9aedab9545d8bf1bbc4cf6
zestbear <zestbear90@gmail.com>
2023-11-20 16:32:50 +0900

    일정 반환 MockMvc 테스트 코드 작성

--- src/test/java/Team4/CalendarNLPServer/controller/ScheduleControllerTest.java
+++ src/test/java/Team4/CalendarNLPServer/controller/ScheduleControllerTest.java
@@ -1,5 +1,6 @@
 package Team4.CalendarNLPServer.controller;
 
+import Team4.CalendarNLPServer.controller.dto.ScheduleListResponseDto;
 import Team4.CalendarNLPServer.controller.dto.ScheduleSaveRequestDto;
 import Team4.CalendarNLPServer.controller.dto.StudentResponseDto;
 import Team4.CalendarNLPServer.domain.schedule.Schedule;
@@ -7,7 +8,9 @@ import Team4.CalendarNLPServer.domain.schedule.ScheduleRepository;
 import Team4.CalendarNLPServer.exceptions.ScheduleNOTExistException;
 import Team4.CalendarNLPServer.service.schedule.ScheduleService;
 import Team4.CalendarNLPServer.service.student.StudentService;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -15,18 +18,20 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockReset;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -128,8 +133,29 @@ class ScheduleControllerTest {
 
     }
 
-    // TODO: 모든 일정 반환 controller 테스트 코드 작성
-    // TODO: 일정 겁색 controller 테스트 코드 작성
+    @Test
+    public void 일정_반환_controllerTest() throws Exception {
+
+        MvcResult mvcResult = mockMvc.perform(
+                        get("/schedule/" + studentId)
+                )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String jsonResponse = mvcResult.getResponse().getContentAsString();
+        List<ScheduleListResponseDto> schedules = new ObjectMapper().readValue(jsonResponse, new TypeReference<List<ScheduleListResponseDto>>(){});
+
+        assertEquals(1, schedules.size());
+        assertEquals("event", schedules.get(0).getEvent());
+        assertEquals("location", schedules.get(0).getLocation());
+        assertEquals("month", schedules.get(0).getMonth());
+        assertEquals("day", schedules.get(0).getDay());
+        assertEquals("time", schedules.get(0).getTime());
+
+    }
+
+
 
     @Test
     public void 일정_삭제_controllerTest() throws Exception {
