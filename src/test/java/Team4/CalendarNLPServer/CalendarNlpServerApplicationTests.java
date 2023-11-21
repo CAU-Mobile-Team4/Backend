7f65c70352ffbb2b496ba86428ed956246cbb410
zestbear <zestbear90@gmail.com>
2023-11-21 11:41:25 +0900

    테스트 환경과 개발 환경 분리

--- src/test/java/Team4/CalendarNLPServer/CalendarNlpServerApplicationTests.java
+++ src/test/java/Team4/CalendarNLPServer/CalendarNlpServerApplicationTests.java
@@ -2,7 +2,9 @@ package Team4.CalendarNLPServer;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class CalendarNlpServerApplicationTests {
 
--- src/test/java/Team4/CalendarNLPServer/controller/StudentControllerTest.java
+++ src/test/java/Team4/CalendarNLPServer/controller/StudentControllerTest.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -17,6 +18,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+@ActiveProfiles("test")
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 @AutoConfigureMockMvc
