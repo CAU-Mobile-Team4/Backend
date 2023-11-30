8a1f5832fe4f25264e80169cda5e514910288cc2
zestbear <zestbear90@gmail.com>
2023-11-30 11:02:13 +0900

    학생 로그인 / 회원가입 기능 분리

--- src/main/java/Team4/CalendarNLPServer/controller/StudentController.java
+++ src/main/java/Team4/CalendarNLPServer/controller/StudentController.java
@@ -11,11 +11,16 @@ public class StudentController {
 
     private final StudentService studentService;
 
-    // 사용자 로그인 및 회원가입
-    // 입력하였을 때 DB에 정보가 있으면 로그인, 정보가 없으면 자동 회원가입
-    @PostMapping("/student")
+    // 학생 회원가입
+    @PostMapping("/join")
     public Long join(@RequestBody StudentResponseDto responseDto) {
         return studentService.join(responseDto);
+    }
+
+    // 학생 로그인
+    @PostMapping("/login")
+    public Long login(@RequestBody StudentResponseDto responseDto) {
+        return studentService.login(responseDto);
     }
 
 }
