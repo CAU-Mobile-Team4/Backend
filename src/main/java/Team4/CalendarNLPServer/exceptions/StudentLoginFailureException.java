be61f79bee7e6f5a653a69160cfcc819ae32d141
zestbear <zestbear90@gmail.com>
2023-11-30 09:22:25 +0900

    학생 로그인 실패 Exception 추가

---
+++ src/main/java/Team4/CalendarNLPServer/exceptions/StudentLoginFailureException.java
@@ -0,0 +1,9 @@
+package Team4.CalendarNLPServer.exceptions;
+
+public class StudentLoginFailureException extends RuntimeException {
+
+    public StudentLoginFailureException(String message) {
+        super(message);
+    }
+
+}
