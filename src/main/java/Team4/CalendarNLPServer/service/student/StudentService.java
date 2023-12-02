c47cad8dc1f10b34b5a9a6888c268155cfd80c35
zestbear <zestbear90@gmail.com>
2023-12-02 17:36:08 +0900

    학생 회원가입 / 로그인 기능 분리

--- src/main/java/Team4/CalendarNLPServer/service/student/StudentService.java
+++ src/main/java/Team4/CalendarNLPServer/service/student/StudentService.java
@@ -3,6 +3,8 @@ package Team4.CalendarNLPServer.service.student;
 import Team4.CalendarNLPServer.controller.dto.StudentResponseDto;
 import Team4.CalendarNLPServer.domain.student.Student;
 import Team4.CalendarNLPServer.domain.student.StudentRepository;
+import Team4.CalendarNLPServer.exceptions.StudentAlreadyExistException;
+import Team4.CalendarNLPServer.exceptions.StudentLoginFailureException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,14 +17,11 @@ public class StudentService {
 
     private final StudentRepository studentRepository;
 
+    // 회원가입
     @Transactional
     public Long join(StudentResponseDto responseDto) {
-        Optional<Student> findStu = studentRepository.findById(responseDto.getId());
-        if (findStu.isPresent()) {
-            if (responseDto.getName().equals(findStu.get().getName())) {
-                return findStu.get().getId();
-            }
-        }
+        findDuplicatedStudent(responseDto.getId());
+
         Student student = Student.builder()
                 .id(responseDto.getId())
                 .name(responseDto.getName())
@@ -30,6 +29,25 @@ public class StudentService {
         studentRepository.save(student);
 
         return student.getId();
+    }
+
+    public void findDuplicatedStudent(Long stuId) {
+        Optional<Student> student = studentRepository.findById(stuId);
+        if (student.isPresent()) {
+            throw new StudentAlreadyExistException("학생이 이미 존재합니다.");
+        }
+    }
+
+    // 로그인
+    @Transactional
+    public Long login(StudentResponseDto responseDto) {
+        Optional<Student> findStu = studentRepository.findById(responseDto.getId());
+        if (findStu.isPresent()) {
+            if (responseDto.getName().equals(findStu.get().getName())) {
+                return findStu.get().getId();
+            }
+        }
+        throw new StudentLoginFailureException("로그인에 실패했습니다.");
     }
 
     public void deleteAll() {
